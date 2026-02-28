c81780822a63e7cfd3b9902c322a6d904432f673
Osama Elnagar <osamaelngar98@gmail.com>
2026-02-28 04:59:48 +0200

    feat: Add console commands and scheduling for daily reports and queue worker management.

--- routes/console.php
+++ routes/console.php
@@ -9,3 +9,4 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Schedule::command('reports:daily-sales')->dailyAt('08:00');
+Schedule::command('queue:work --stop-when-empty')->everyMinute()->withoutOverlapping();
